61cbfea712c8546acb693f022862c3acafdbaa37
micabuenahora <micaelabuenahora02@gmail.com>
2023-09-14 22:46:22 -0300

    1erpreentrega+micaelabuenahora

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import  "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Titulo } from "./components/ItemListContainer"
+
+function header() {
+
+
+  return (
+    <div>
+          <Navbar/> 
+          <Titulo/>
+    </div>
+     
+  );
+
+
+}  
+
+
+
+  
+
+export default header;
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,14 @@
+export const Titulo = () => {
+    let stylesTitulo ={
+        display: 'flex',
+        justifyContent:'center',
+    }
+    return (
+
+        <h1 style={stylesTitulo}>!BIENVENIDOS A PATAGONIA INVESTING!</h1>
+
+    )
+     
+    
+}
+
